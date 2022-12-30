c32f8b3a0cc5c28688bbb2ab8003636c0402eb46
AvPKing <>
2022-12-30 13:37:32 +0100

    Made a low memory usage version of the script. Added progress bars for low memory version. Added execution time measurements.

--- Asura.py
+++ Asura.py
@@ -1,5 +1,6 @@
 import zlib
 import io
+import hashlib
 
 def int32(input):
     return input & 0xffffffff
@@ -8,6 +9,105 @@ def parseInt32(bytesIO):
     intBytes = bytesIO.read(4)
     return int.from_bytes(intBytes, 'little')
     
+
+class ZlibBytesIO:
+    def __init__(self, io):
+        self.io = io
+        self.buffer = b''
+        self.CHUNK_SIZE = 1024
+        self.decompData = b''
+        self.decompObj = zlib.decompressobj(13)
+        self.compObj = zlib.compressobj(level=zlib.Z_DEFAULT_COMPRESSION, method=zlib.DEFLATED, wbits=13, memLevel=9, strategy=zlib.Z_DEFAULT_STRATEGY)
+    
+    def read(self, nBytes):
+        while len(self.decompData) < nBytes:
+            self.buffer = self.io.read(self.CHUNK_SIZE)
+            if not self.buffer:
+                break
+            else:
+                self.decompData = self.decompData + self.decompObj.decompress(self.buffer)
+        result = self.decompData[:nBytes]
+        self.decompData = self.decompData[nBytes:]
+        return result
+
+    def write(self, bytes):
+        compressedBytes = self.compObj.compress(bytes)
+        compressedBytes = compressedBytes + self.compObj.flush(zlib.Z_NO_FLUSH)
+        self.io.write(compressedBytes)
+        return len(compressedBytes)
+
+    def close(self):
+        self.io.write(self.compObj.flush())
+
+
+class AsuraFileReader:
+    def __init__(self, filename):
+        self.file = open(filename, 'rb')
+        self.file.seek(0x10, 0)
+        self.toRead = int.from_bytes(self.file.read(4), 'little')
+        self.chunkDataRead = 0
+        self.file.seek(0x14, 0)
+        self.zlb = ZlibBytesIO(self.file)
+
+    def getPercentageRead(self):
+        return (self.chunkDataRead/self.toRead) * 100
+
+    def getNextChunk(self):
+        if self.file.tell() == 0x14:
+            header = self.zlb.read(8)
+            self.chunkDataRead = self.chunkDataRead + 8
+            return self.getNextChunk()
+        else:
+            chunkID = self.zlb.read(4)
+            if chunkID == b'\x00\x00\x00\x00' or b'':
+                return b''
+            szChunk = int.from_bytes(self.zlb.read(4), 'little')
+            self.chunkDataRead = self.chunkDataRead + szChunk
+            toReturn = chunkID + szChunk.to_bytes(4, 'little') + self.zlb.read(szChunk - 0x8)
+            return toReturn
+            
+class AsuraFileWriter:
+    def __init__(self, filename):
+        self.file = open(filename, 'wb')
+        self.zlb = ZlibBytesIO(self.file)
+        self.file.write(b'AsuraZlb')
+        self.file.write(b'\x00\x00\x00\x00')
+        self.uncompressedSize = 8
+        self.compressedSize = 0
+        self.file.write(b'\x00\x00\x00\x00')
+        self.file.write(b'\x00\x00\x00\x00')
+        self.compressedSize = self.zlb.write(b'Asura   ')
+        currPos = self.file.tell()
+        self.file.seek(0xC, 0)
+        self.file.write(self.compressedSize.to_bytes(4, 'little'))
+        self.file.write(self.uncompressedSize.to_bytes(4, 'little'))
+        self.file.seek(currPos, 0)
+        self.chunkHashList = []
+
+    def chunkExists(self, chunk):
+        chunkHash = hashlib.md5(chunk).digest()
+        return chunkHash in self.chunkHashList
+
+    def writeChunk(self, chunk):
+        chunkHash = hashlib.md5(chunk).digest()
+        if chunkHash in self.chunkHashList:
+            return
+        nWritten = self.zlb.write(chunk)
+        if nWritten > 0:
+            self.chunkHashList.append(chunkHash)
+        curFPos = self.file.tell()
+        self.file.seek(0xc,0)
+        self.uncompressedSize = self.uncompressedSize + int.from_bytes(chunk[4:8], 'little')
+        self.compressedSize = self.compressedSize + nWritten
+        self.file.write(self.compressedSize.to_bytes(4, 'little'))
+        self.file.write(self.uncompressedSize.to_bytes(4, 'little'))
+        self.file.seek(curFPos, 0)
+
+    def __del__(self):
+        self.zlb.write(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        self.zlb.close()
+
+
 def getAsrFileAsBytesIO(f):
     asrType = f.read(8)
     if asrType == b'AsuraZlb':
--- MergeCharacters.py
+++ MergeCharacters.py
@@ -1,6 +1,7 @@
 import Asura
 import os
 import re
+import time
 
 def filterCharacters(chunkDict):
     toRemove = []
@@ -70,6 +71,62 @@ def filterCharacters(chunkDict):
         chunkDict.pop(key)
     return materialRSCFList
 
+def filterCharactersPredicate(chunk):
+    key = chunk[0:4]
+    if key == b'FNFO':
+        return False
+    if key == b'RSFL':
+        return False
+    if key == b'RSCF':
+        filename = Asura.readPaddedByteStr(chunk, 28)
+        if(filename == b'Material Response Texture'):
+            return True
+        else:
+            return False
+    if key == b'SDSM':
+        return False
+    if key == b'SDEV':
+        return False
+    if key == b'DLEV':
+        return False
+    if key == b'HANM':
+        return False
+    if key == b'FACE':
+        return False
+    if key == b'FAAN':
+        return False
+    if key == b'MTRL':
+        return False
+    if key == b'HSKN':
+        return False
+    if key == b'HSKL':
+        return False
+    if key == b'HMPT':
+        return False
+    if key == b'HSND':
+        return False
+    if key == b'HSBB':
+        return False
+    if key == b'FXPT':
+        return False
+    if key == b'FXST':
+        return False
+    if key == b'FXET':
+        return False
+    if key == b'FSX2':
+        return False
+    if key == b'TXAN':
+        return False
+    # if key == b'apas':
+    #    return False
+    # if key == b'hpas':
+    #    return False
+    # if key == b'ppas':
+    #    return False
+    if key == b'MARE':
+        return False
+    return True
+
 def filterOnlyMares(chunkDict):
     toRemove = []
     for key in chunkDict:
@@ -95,11 +152,51 @@ def mergeChunkDict (firstDict, secondDict):
                 if not found:
                     firstDict[key].append(chunk)
 
-def main():
+def lowMemoryProcess():
     AvPFolder = input("Enter the root AvP folder: ")
+    dest = os.path.join(AvPFolder, 'Characters/All_Characters.asr')
+    AllCharacters = Asura.AsuraFileWriter(dest)
+    print('Processing, this can take a few minutes...')
+    start = time.time()
+    for dirpath, dirnames, filenames in os.walk(AvPFolder):
+        for file in filenames:
+            regex = '([A,M,P]0[0-9]_[\\w]+\\.pc)|(Multiplayer.asr)'
+            fullName = os.path.join(dirpath, file)
+            if re.search(regex, file):
+                if dirpath.endswith('/MP') or dirpath.endswith('\\MP') or ('P00_Tutorial' in file):
+                    continue
+                else:
+                    print(f'File found: {fullName}. Extracting characters...', end='\r')
+                    asrFile = Asura.AsuraFileReader(fullName)
+                    nextChunk = asrFile.getNextChunk()
+                    while nextChunk != b'':
+                        print(f'File found: {fullName}. Extracting characters... {round(asrFile.getPercentageRead(), 2)}%', end='\r')
+                        if (not filterCharactersPredicate(nextChunk)):
+                            AllCharacters.writeChunk(nextChunk)
+                        nextChunk = asrFile.getNextChunk()
+                    print('')
+    mplst = open(os.path.join(AvPFolder, 'Characters/Multiplayer.lst'), "w")
+    mplst.write('Characters/All_Characters.asr\n')
+    mplst.write('Characters/Multiplayer.en\n')
+    end = time.time()
+    totalTimeElapsed = round((end - start)/60, 2)
+    print('Done.')
+    print(f'Total execution time: {totalTimeElapsed} minutes')
+
+
+
+def main():
+    lowMemoryPC = input('Does your pc have less than 4 gigabytes of RAM? (Yes/No): ')
+    if lowMemoryPC.lower() == 'yes':
+        print('Activating low memroy mode...')
+        lowMemoryProcess()
+        return
+    AvPFolder = input("Enter the root AvP folder: ")
+    dest = os.path.join(AvPFolder, 'Characters/All_Characters.asr')
     chunkDict = {}
     mareDict = {}
     print('Processing, this can take a few minutes...')
+    start = time.time()
     for dirpath, dirnames, filenames in os.walk(AvPFolder):
         for file in filenames:
             regex = '([A,M,P]0[0-9]_[\\w]+\\.pc)|(Multiplayer.asr)'
@@ -122,12 +219,14 @@ def main():
                     mareTextureFile[b'RSCF'] = [MARETexture]
                     mareDict[file] = mareTextureFile
                     mergeChunkDict(chunkDict, temp)
-    dest = os.path.join(AvPFolder, 'Characters/All_Characters.asr')
     Asura.exportChunkDict(dest, chunkDict)
     mplst = open(os.path.join(AvPFolder, 'Characters/Multiplayer.lst'), "w")
     mplst.write('Characters/All_Characters.asr\n')
     mplst.write('Characters/Multiplayer.en\n')
+    end = time.time()
+    totalTimeElapsed = round((end - start)/60, 2)
     print('Done.')
+    print(f'Total execution time: {totalTimeElapsed} minutes')
 
 
 
